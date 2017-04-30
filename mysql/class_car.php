41ac3fd20552d4590455edcdef58d07cda2c8c2a
John Currie <johntcurrie@gmail.com>
2017-04-30 15:38:39 -0700

    Added properties to the Car class

--- mysql/class_car.php
+++ mysql/class_car.php
@@ -1,3 +1,15 @@
+<?php
+  class Car {
+  	var $wheels = 4;
+  	var $hood = 1;
+  	var $engine = 1;
+  	var $doors = 2; 
+  	function MoveWheels() {
+      $doors = 6;
+  	}
+  	echo "$doors";
+  }
+?>
 <!DOCTYPE html>
 <html>
 <head>
